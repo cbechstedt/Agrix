079e974ee535d0b1d26af3a3e390658381890388
Christian <chrisnebech@hotmail.com>
2023-09-21 16:37:58 -0300

    requisito 3

---
+++ src/main/java/com/betrybe/agrix/dto/AuthDto.java
@@ -0,0 +1,8 @@
+package com.betrybe.agrix.dto;
+
+/**
+ * javadoc.
+ */
+public record AuthDto(String username, String password) {
+
+}
---
+++ src/main/java/com/betrybe/agrix/dto/TokenDto.java
@@ -0,0 +1,8 @@
+package com.betrybe.agrix.dto;
+
+/**
+ * Define DTO para token.
+ */
+public record TokenDto(String token) {
+
+}
