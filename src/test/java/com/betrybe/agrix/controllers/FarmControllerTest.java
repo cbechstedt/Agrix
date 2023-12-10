d1b7012841665e8f3e49c875d13b623c7e2cb610
Christian <chrisnebech@hotmail.com>
2023-12-10 19:02:44 -0300

    add FarmServiceTest

---
+++ src/test/java/com/betrybe/agrix/controllers/FarmControllerTest.java
@@ -0,0 +1,20 @@
+package com.betrybe.agrix.controllers;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class FarmControllerTest {
+
+  @Test
+  void save() {
+  }
+
+  @Test
+  void findAll() {
+  }
+
+  @Test
+  void findById() {
+  }
+}
---
+++ src/test/java/com/betrybe/agrix/services/FarmServiceTest.java
@@ -0,0 +1,20 @@
+package com.betrybe.agrix.services;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class FarmServiceTest {
+
+  @Test
+  void save() {
+  }
+
+  @Test
+  void findAll() {
+  }
+
+  @Test
+  void findById() {
+  }
+}
